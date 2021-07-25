893ce747dcab1281d360d5dc2237543fda01b7fe
charmingsong <web_songsong@163.com>
2021-07-25 21:09:32 +0800

    test

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import hintTemplate from './commands/hint-template';
-import updateTemplate from './commands/update-template';
+import hintTemplate from "./commands/hint-template";
+import updateTemplate from "./commands/update-template";
 
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vscodecs" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscodecs" is now active!');
+  let disposableUpdateTemplate = vscode.commands.registerCommand(
+    "vscodecs.updateTemplate",
+    updateTemplate
+  );
 
-	let disposableUpdateTemplate = vscode.commands.registerCommand('vscodecs.updateTemplate',async ()=> updateTemplate());
+  let disposableHintTemplate = vscode.commands.registerCommand(
+    "vscodecs.hintTemplate",
+    hintTemplate
+  );
 
-	let disposableHintTemplate = vscode.commands.registerCommand('vscodecs.hintTemplate', hintTemplate);
-
-
-	context.subscriptions.push(disposableUpdateTemplate, disposableHintTemplate);
+  context.subscriptions.push(disposableUpdateTemplate, disposableHintTemplate);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
